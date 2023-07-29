2857c10a3d3085a6e430e61e2bf9bfcba9688f3e
juharyimami <juharyimami2011@gmail.com>
2023-07-29 22:17:40 +0300

    user page created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useSelector } from 'react-redux';
+import AuthenticatedRoute from './routes/AuthenticatedRoute';
+import UnAuthenticatedRoute from './routes/UnAuthenticatedRoute';
 
 function App() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {!isAuthenticated ? (
+        <UnAuthenticatedRoute />
+      ) : (
+        <AuthenticatedRoute />
+      )}
     </div>
   );
 }
